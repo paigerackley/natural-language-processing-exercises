3d839e6ca387d9cd6a7a9e4019205d6311b215fc
paigerackley <102173123+paigerackley@users.noreply.github.com>
2022-07-20 14:48:16 -0500

    11

--- acquire.py
+++ acquire.py
@@ -1,83 +1,95 @@
 from requests import get
 from bs4 import BeautifulSoup
-import os
+import numpy as np
 import pandas as pd
-import requests 
+import os
 
 
-#######################################################
-# This file is for the acquire exercises for NLP codeup module.
-#######################################################
 
-def get_blog_posts(refresh=False):
+def get_blog_urls():
+    headers = {'user-agent': 'Innis Codeup Data Science'}
+    response = get('https://codeup.com/blog/', headers=headers)
+    soup = BeautifulSoup(response.text, features="lxml")
+    urls = [a.attrs['href'] for a in soup.select('a.more-link')]
+    return urls
     
-    if not os.path.isfile('blog_articles.csv') or refresh:
-        
-        url = 'https://codeup.com/blog/'
-        headers = {'User-Agent': 'Codeup Data Science'}
-        response = get(url, headers=headers)
 
-        soup = BeautifulSoup(response.content, 'html.parser')
-
-        links = [link['href'] for link in soup.select('h2 a[href]')]
-
-        articles = []
-
-        for url in links:
-
-            url_response = get(url, headers=headers)
-            soup = BeautifulSoup(url_response.text, 'html.parser')
-
-            title = soup.find('h1', class_='entry-title').text
-            content = soup.find('div', class_='entry-content').text.strip()
-
-            article_dict = {
-                'title': title,
-                'content': content
-            }
-
-            articles.append(article_dict)
-        
-        blog_article_df = pd.DataFrame(articles)
-        
-        blog_article_df.to_csv('blog_articles.csv', index=False)
-        
-    return pd.read_csv('blog_articles.csv')
-
-def get_news_articles(refresh=False):
+def parse_blog(url):
+    url = url
+    #establish header
+    headers = {'User-Agent': 'Codeup Data Science'}
+    response = get(url, headers=headers)
     
-    if not os.path.isfile('news_articles.csv') or refresh:
+    # Make a soup variable holding the response content
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    output = {}
+    output['title'] = soup.find('h1', class_='entry-title').text
+    output['date'] = soup.find('span', class_='published').text
+    output['category'] = soup.find('a', rel = 'category tag').text
+    output['content'] = soup.find('div', class_= 'entry-content').text.strip().replace('\n', ' ').replace('\xa0', ' ')
+    
+    return output
+
+
+
+def get_blog_articles(use_cache=True):
+    
+    if os.path.exists('blog_articles.json') and use_cache:
+        return pd.read_json('blog_articles.json')
+
+    urls = get_blog_urls()
+    output = []
+    
+    for url in urls:
+        output.append(parse_blog(url))
         
-        url = 'https://inshorts.com/en/read'
-        response = get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        
-        categories = [li.text.lower() for li in soup.select('li')][1:]
-        categories[0] = 'national'
+    df = pd.DataFrame(output)
+    df.to_json('blog_articles.json', orient='records')
+    
+    return df
 
-        inshorts = []
 
-        for category in categories:
+def parse_news_article(article, category):
+    output = {}
 
-            cat_url = url + '/' + category
-            response = get(url)
-            soup = BeautifulSoup(response.content, 'html.parser')
+    output['category'] = category
+    output['title'] = article.find('span', itemprop = 'headline').text.strip()
+    output['author'] = article.find('span', class_ = 'author').text
+    output['date'] = article.find('span', clas = 'date').text.split(',')[0]
+    output['content'] = article.find('div', itemprop = 'articleBody').text
 
-            titles = [span.text for span in soup.find_all('span', itemprop='headline')]
-            contents = [div.text for div in soup.find_all('div', itemprop='articleBody')]
+    return output
 
-            for i in range(len(titles)):
 
-                article = {
-                    'title': titles[i],
-                    'content': contents[i],
-                    'category': category,
-                }
 
-                inshorts.append(article)
-                
-        inshorts_article_df = pd.DataFrame(inshorts)
-        
-        inshorts_article_df.to_csv('news_articles.csv', index=False)
-                
-    return pd.read_csv('news_articles.csv')
+def parse_news_page(category):
+    url = 'https://inshorts.com/en/read/' + category
+    response = get(url)
+    soup = BeautifulSoup(response.text)
+
+    cards = soup.select('.news-card')
+    articles = []
+
+    for card in cards:
+        articles.append(parse_news_article(card, category))
+
+    return articles
+
+
+
+# cache the data, and turn it into a dataframe (function)
+def get_news_articles(use_cache=True):
+    if os.path.exists('news_articles.json') and use_cache:
+        return pd.read_json('news_articles.json')
+
+    categories = ['business', 'sports', 'technology', 'entertainment']
+
+    articles = []
+
+    for category in categories:
+        articles.extend(parse_news_page(category))
+
+    df = pd.DataFrame(articles)
+    df.to_json('news_articles.json', orient='records')
+    return df
